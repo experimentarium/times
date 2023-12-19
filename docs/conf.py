a53645ec11173fb0e1ee164087697978e125242a
Olexandr Balyk <o.balyk@columbia.edu>
2023-12-18 21:44:00 -0500

    Enable numref

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ rst_prolog = """.. attention::
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["myst_parser"]
+numfig = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
